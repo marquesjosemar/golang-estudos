1d7eb57671627319ce3be84275beff5eb75ab731
marquesjosemar <126707247+marquesjosemar@users.noreply.github.com>
2025-09-29 21:12:43 -0300

    função make

---
+++ make/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	minhaCasa := make([]string, 3) //Construa uma casa com 3 comodos
+
+	minhaCasa[0] = "sala"
+	minhaCasa[1] = "Quarto"
+	minhaCasa[2] = "Cozinha"
+
+	fmt.Println(minhaCasa)
+}
